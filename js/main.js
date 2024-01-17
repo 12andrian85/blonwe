bdeb2f422f61821c53de7e275486946421c63ac6
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2024-01-17 12:35:29 +0200

    add layout for shop page

--- js/main.js
+++ js/main.js
@@ -9,76 +9,136 @@ const splideSeafood = document.querySelector('#seafood')
 
 if (splideCategories) {
 
-    new Splide('#categories', {
-      type: 'loop',
-      perPage: 3,
-      // autoplay: 'true',
-      // focus: 'center',
-      arrows: true,
-      breakpoints: {
-        640: {
-          perPage: 1,
-        },
-        768: {
-          perPage: 6,
-        },
-        1200: {
-          perPage: 8,
-        },
-        2560: {
-          perPage: 9,
-        },
-      }
-    }).mount();
-  }
+  new Splide('#categories', {
+    type: 'loop',
+    perPage: 3,
+    // autoplay: 'true',
+    // focus: 'center',
+    arrows: true,
+    breakpoints: {
+      640: {
+        perPage: 1,
+      },
+      768: {
+        perPage: 6,
+      },
+      1200: {
+        perPage: 8,
+      },
+      2560: {
+        perPage: 9,
+      },
+    }
+  }).mount();
+}
 
 if (splideSales) {
 
-    new Splide('#sales', {
-      type: 'loop',
-      perPage: 3,
-      // autoplay: 'true',
-      // focus: 'center',
-      arrows: true,
-      breakpoints: {
-        640: {
-          perPage: 2,
-        },
-        768: {
-          perPage: 3,
-        },
-        1200: {
-          perPage: 6,
-        },
-        2560: {
-          perPage: 6,
-        },
-      }
-    }).mount();
-  }
-
-  // if (splideSeafood) {
-
-  //   new Splide('#seafood', {
-  //     type: 'loop',
-  //     perPage: 3,
-  //     // autoplay: 'true',
-  //     // focus: 'center',
-  //     arrows: true,
-  //     breakpoints: {
-  //       640: {
-  //         perPage: 2,
-  //       },
-  //       768: {
-  //         perPage: 3,
-  //       },
-  //       1200: {
-  //         perPage: 6,
-  //       },
-  //       2560: {
-  //         perPage: 6,
-  //       },
-  //     }
-  //   }).mount();
-  // }
+  new Splide('#sales', {
+    type: 'loop',
+    perPage: 3,
+    // autoplay: 'true',
+    // focus: 'center',
+    arrows: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+      },
+      768: {
+        perPage: 3,
+      },
+      1200: {
+        perPage: 6,
+      },
+      2560: {
+        perPage: 6,
+      },
+    }
+  }).mount();
+}
+  // input-number
+// $('.quantity-button').off('click').on('click', function () {
   
+//   if ($(this).hasClass('quantity-add')) {
+//     var addValue = parseInt($(this).parent().find('input').val()) + 1;
+// 		$(this).parent().find('input').val(addValue).trigger('change');
+// 	}
+
+// 	if ($(this).hasClass('quantity-remove')) {
+//     var removeValue = parseInt($(this).parent().find('input').val()) - 1;
+// 		if( removeValue == 0 ) {
+//       removeValue = 1;
+// 		}
+// 		$(this).parent().find('input').val(removeValue).trigger('change');
+// 	}
+
+// });
+
+
+
+
+// if (splideSeafood) {
+
+//   new Splide('#seafood', {
+//     type: 'loop',
+//     perPage: 3,
+//     // autoplay: 'true',
+//     // focus: 'center',
+//     arrows: true,
+//     breakpoints: {
+//       640: {
+//         perPage: 2,
+//       },
+//       768: {
+//         perPage: 3,
+//       },
+//       1200: {
+//         perPage: 6,
+//       },
+//       2560: {
+//         perPage: 6,
+//       },
+//     }
+//   }).mount();
+// }
+
+
+var splide = new Splide( '#main-carousel', {
+  pagination: false,
+  focus: 'center',
+} );
+
+
+var thumbnails = document.getElementsByClassName( 'thumbnail' );
+var current;
+
+
+for ( var i = 0; i < thumbnails.length; i++ ) {
+  initThumbnail( thumbnails[ i ], i );
+}
+
+
+function initThumbnail( thumbnail, index ) {
+  thumbnail.addEventListener( 'click', function () {
+    splide.go( index );
+  } );
+}
+
+
+splide.on( 'mounted move', function () {
+  var thumbnail = thumbnails[ splide.index ];
+
+
+  if ( thumbnail ) {
+    if ( current ) {
+      current.classList.remove( 'is-active' );
+    }
+
+
+    thumbnail.classList.add( 'is-active' );
+    current = thumbnail;
+  }
+} );
+
+
+splide.mount();

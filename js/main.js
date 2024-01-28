f4220bcae042cdd4ad971a0b69e39a4431ff7996
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2024-01-28 02:54:35 +0200

    add styles for shop page

--- js/main.js
+++ js/main.js
@@ -105,42 +105,44 @@ if (splideSales) {
 // }
 
 
-var splide = new Splide('#main-carousel', {
+var splide = new Splide( '#main-carousels', {
   pagination: false,
-  focus: 'center',
-});
+} );
 
 
-var thumbnails = document.getElementsByClassName('thumbnail');
+var thumbnails = document.getElementsByClassName( 'thumbnail' );
 var current;
 
 
-for (var i = 0; i < thumbnails.length; i++) {
-  initThumbnail(thumbnails[i], i);
+for ( var i = 0; i < thumbnails.length; i++ ) {
+  initThumbnail( thumbnails[ i ], i );
 }
 
 
-function initThumbnail(thumbnail, index) {
-  thumbnail.addEventListener('click', function () {
-    splide.go(index);
-  });
+function initThumbnail( thumbnail, index ) {
+  thumbnail.addEventListener( 'click', function () {
+    splide.go( index );
+  } );
 }
 
 
-splide.on('mounted move', function () {
-  var thumbnail = thumbnails[splide.index];
+splide.on( 'mounted move', function () {
+  var thumbnail = thumbnails[ splide.index ];
 
 
-  if (thumbnail) {
-    if (current) {
-      current.classList.remove('is-active');
+  if ( thumbnail ) {
+    if ( current ) {
+      current.classList.remove( 'is-active' );
     }
 
 
-    thumbnail.classList.add('is-active');
+    thumbnail.classList.add( 'is-active' );
     current = thumbnail;
   }
-});
+} );
 
 
 splide.mount();
+
+
+  
